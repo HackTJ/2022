8296d184e9ff81ac26b712575a8a8ddaf480646f
Autin Mitra <autin123@gmail.com>
2022-03-03 19:26:14 -0500

    resources component

--- src/components/Resources/ResourceCard.svelte
+++ src/components/Resources/ResourceCard.svelte
@@ -1,5 +1,17 @@
 <script>
-  export let href = ""
+  export let title, description, href;
 </script>
 
-<div></div>
+<div class="p-6 bg-blueberry-200 text-blackberry-800 rounded-2xl flex flex-col justify-between">
+  <div>
+    <h1 class="font-bold text-2xl">{title}</h1>
+    <p class="mt-2">{description}</p>
+  </div>
+  <a 
+    href="{href}"
+    target="_blank"
+    rel="noopener noreferred"
+    class="block mt-4 text-center py-4 px-6 font-bold rounded-xl bg-black text-white w-full">
+    Open Resource
+  </a>
+</div>
