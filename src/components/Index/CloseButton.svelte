b57b63963c4d04b54661546faae5df30f0b5c086
Sumanth Ratna <sumanth@hacktj.org>
2022-03-11 09:59:14 -0500

    Fix home icon link
    This commit closes https://github.com/HackTJ/2022/issues/24.

--- src/components/Index/CloseButton.svelte
+++ src/components/Index/CloseButton.svelte
@@ -1,7 +1,11 @@
 <script>
-  import Close from '@svicons/ionicons-solid/close.svelte';
+	import Close from '@svicons/ionicons-solid/close.svelte';
 
-  export let onClose;
+	export let onClose;
 </script>
 
-<button on:click={onClose} class="absolute right-0 mr-6 md:mr-8 rounded-xl bg-black text-theme flex items-center justify-center w-12 h-12 text-center"><Close width="32px" /></button>
+<button
+	on:click={onClose}
+	class="absolute right-0 mr-6 md:mr-8 rounded-xl bg-black text-theme flex items-center justify-center w-12 h-12 text-center"
+	><Close width="32px" /></button
+>
