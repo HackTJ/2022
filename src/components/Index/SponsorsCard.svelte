d559605aef54cdfe630dd691c98c9b01b9ba6a7b
Autin Mitra <autin123@gmail.com>
2022-03-24 00:11:35 -0400

    hover sponsor

--- src/components/Index/SponsorsCard.svelte
+++ src/components/Index/SponsorsCard.svelte
@@ -8,7 +8,7 @@
 	{href}
 	target="_blank"
 	rel="noopener"
-	class="w-[225px] h-[125px] flex items-center flex-row mt-2 bg-black p-8 rounded-2xl shrink-0"
+	class="w-[225px] h-[125px] flex items-center flex-row mt-2 bg-black p-8 rounded-2xl shrink-0 hover:brightness-75"
 >
 	<img {alt} src={srcLogo} />
 </a>
